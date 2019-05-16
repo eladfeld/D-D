bf67615e34a7b21ff331fe51172ceb113315afae
Elad Feldman <eladfeld@post.bgu.ac.il>
2019-05-16 16:17:20 +0300

    lifebar addded

--- D&Dgame/src/Proccesor.java
+++ D&Dgame/src/Proccesor.java
@@ -3,20 +3,20 @@ import java.io.File;
 import java.io.FileReader;
 
 public class Proccesor {
-    public static GUnit[][] levelProccesor(File file , int height){
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st = br.readLine();
-        int length = st.length();
-        GUnit[][] result = new char[height][length];
-        for(int i = 0; i < height ; i++) {
-            st = br.readLine();
-        }
-        return result;
+//    public static GUnit[][] levelProccesor(File file , int height){
+//        BufferedReader br = new BufferedReader(new FileReader(file));
+//        String st = br.readLine();
+//        int length = st.length();
+//        GUnit[][] result = new char[height][length];
+//        for(int i = 0; i < height ; i++) {
+//            st = br.readLine();
+//        }
+//        return result;
+//
+//    }
 
-    }
-
-    public String lifeBar(int HP , int currHp){
-        int Hpresentage = 100*(currHp/HP);
+    public static String lifeBar(double HP , double currHp){
+        double Hpresentage = 100*currHp/HP;
         String output = "";
         for(int i = 0;i<100-Hpresentage-1;i++){
             output = output + ".";
@@ -26,6 +26,8 @@ public class Proccesor {
             output = "|" + output;
         }
         output= "[" + output;
+        return  output.substring(0,48) + (int)Hpresentage + "%" + output.substring(51);
+
     }
 
 
---
+++ D&Dgame/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        System.out.print(Proccesor.lifeBar(4,3));
+        System.out.print(Proccesor.lifeBar(4,3).length());
+
+
+    }
+}
+
