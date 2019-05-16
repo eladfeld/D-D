43cb240e010946c1bf8040d6bbd75a3750796ce1
Elad Feldman <eladfeld@post.bgu.ac.il>
2019-05-17 00:07:41 +0300

    data added

---
+++ D&Dgame/src/LogicLayer/gameLogic.java
@@ -0,0 +1,12 @@
+package LogicLayer;
+
+import InterfaceLayer.Moves.*;
+import java.io.File;
+
+public class gameLogic {
+   ActionReader  playerMove;
+   RandomGenerator enemyMove;
+   File board;
+
+
+}
