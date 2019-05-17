54e32a1b3eabcbe77e6566070a0d4e4a3ceb4498
Tzuri <teshuba@post.bgu.ac.il>
2019-05-17 22:26:39 +0300

    level processor update

--- D&Dgame/src/LogicLayer/Enemies/Monster.java
+++ D&Dgame/src/LogicLayer/Enemies/Monster.java
@@ -3,7 +3,7 @@ package LogicLayer;
 public class Monster extends Enemy {
 	
 	
-    public Enemy(int x, int y,String name ,int HP ,int DP , int AP , int expValue , char tile, int inSight ){
+    public Monster(int x, int y,String name ,int HP ,int DP , int AP , int expValue , char tile, int inSight ){
         super(x,y,name,HP,DP,AP, expValue, tile, inSight);
     }
 
--- D&Dgame/src/LogicLayer/gameLogic.java
+++ D&Dgame/src/LogicLayer/gameLogic.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 
+
 public class gameLogic {
    private ActionReader  playerMove;
    private RandomGenerator enemyMove;
@@ -24,8 +25,8 @@ public class gameLogic {
       activeGame = true;
       playerMove=AR;
       enemyMove=RG;
-      board=levelProccesor(char[][] level);
       player = askForPlayerType(AR);
+      board=levelProccesor(level, player);
    }
 
    private Player askForPlayerType(ActionReader ar) {
@@ -57,42 +58,45 @@ public class gameLogic {
       return output;
    }
 
-   private gameObject[][] levelProccesor(char[][] board) {
+   private gameObject[][] levelProccesor(char[][] board, Player player) {
       char c;
 	  int width =  board.length;
 	  int length = board[0].length;
-	  GameObject[][] output = new GameObject[width][length];
+	  gameObject[][] output = new gameObject[width][length];
       for(int i=0 ; i<width ; i++) {
     	  for(int j=0 ; j<length ; j++) {
     		  c = board[i][j];
-    		  GameObject GO;
-    		  swicth(c){
-    			  case '.': GO = new FreeSpace(i,j);
+    		  gameObject GO;
+    		  switch(c){
+    			  case '.': GO = new FreeSpace(i,j); break;
     			  case '#': GO =  new Wall(i,j);
-    			  case '@': GO =  new Player(i,j);
-    			  case 'X': GO =  new DeadPlayer(i,j);
+    			  case '@': player.setX(i);
+    			  			player.setY(j);
+    			  			GO = player;
+    			  			break;
+    			  //have function receive parameter for player so it knows how to point at it
+    			  case 'X': GO =  new DeadPlayer(i,j); break;
     			  
-    			  case 's': GO =  new Monster(i,j,"Lannister Soldier", 80, 3, 8, 25, c, 3);
-    			  case 'k': GO =  new Monster(i,j,"Lannister Knight", 200, 8, 14, 50, c, 4);
-    			  case 'q': GO =  new Monster(i,j,"Queen's Guard",400, 15, 20,100, c, 5);
-    			  case 'z': GO =  new Monster(i,j, "Wright", 600, 15, 30, 100, c, 3);
-    			  case 'b': GO =  new Monster(i,j, "Bear Wright", 1000, 30, 75, 250, c, 4);
-    			  case 'g': GO =  new Monster(i,j, "Giant Wright", 1500, 40, 100, 500, c, 5);
-    			  case 'w': GO =  new Monster(i,j," White Walker", 2000, 50, 150, 1000, c, 6);
-    			  case 'M': GO =  new Monster(i,j,"The Mountain", 1000, 25, 60, 500, c, 6);
-    			  case 'C': GO =  new Monster(i,j, "Queen Cersei", 100, 10, 10, 1000, c, 1);
-    			  case 'K': GO =  new Monster(i,j, "Night's King", 5000, 150, 300, 5000, c, 8);
+    			  case 's': GO =  new Monster(i,j,"Lannister Soldier", 80, 3, 8, 25, c, 3, output); break;
+    			  case 'k': GO =  new Monster(i,j,"Lannister Knight", 200, 8, 14, 50, c, 4, output); break;
+    			  case 'q': GO =  new Monster(i,j,"Queen's Guard",400, 15, 20,100, c, 5, output); break;
+    			  case 'z': GO =  new Monster(i,j, "Wright", 600, 15, 30, 100, c, 3, output); break;
+    			  case 'b': GO =  new Monster(i,j, "Bear Wright", 1000, 30, 75, 250, c, 4, output); break;
+    			  case 'g': GO =  new Monster(i,j, "Giant Wright", 1500, 40, 100, 500, c, 5, output); break;
+    			  case 'w': GO =  new Monster(i,j," White Walker", 2000, 50, 150, 1000, c, 6, output); break;
+    			  case 'M': GO =  new Monster(i,j,"The Mountain", 1000, 25, 60, 500, c, 6, output); break;
+    			  case 'C': GO =  new Monster(i,j, "Queen Cersei", 100, 10, 10, 1000, c, 1, output); break;
+    			  case 'K': GO =  new Monster(i,j, "Night's King", 5000, 150, 300, 5000, c, 8, output); break;
     			  
-    			  case 'B': GO =  new Trap(i,j,"Bonus Trap", 1, 1, 1, c, 26);
-    			  case 'Q': GO =  new QueensTrap(i,j);
-    			  case 'D': GO =  new DeathTrap(i,j);
+    			  case 'B': GO =  new Trap(i,j,"Bonus Trap", 1, 1, 1, c, 26, output); break;
+    			  case 'Q': GO =  new Trap(i,j,"Queen's Trap", 250, 10, 50, 100, c, 4, 5, 6, 2, output); break;
+    			  case 'D': GO =  new Trap(i,j, "Death Trap", 500, 20, 100, 250, c, 6, 10, 3, output); break;
     		  }
     		  output[i][j] = GO;
-    		  if(GO.isEnemy()) {} //add to enemy list
+    		  if(GO.isEnemy()) enemies.add(GO); //adds to enemy list
     	  }
       }
-	   //need to implement!!!!
-      return null;
+	  return output;
    }
 
    public void gameTick() {
