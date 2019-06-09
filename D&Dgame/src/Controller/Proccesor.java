fbf15aa90cac59b232e24eb482e8985cc6ec058e
Elad Feldman <eladfeld@post.bgu.ac.il>
2019-06-09 10:16:47 +0300

    premerging

--- D&Dgame/src/Controller/Proccesor.java
+++ D&Dgame/src/Controller/Proccesor.java
@@ -8,59 +8,56 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Proccesor{
+public class Proccesor {
 
-    public static int[] tickProccesor(String path){
+    public static int[] tickProccesor(String path) {
         Path P = Paths.get(path);
         File file = new File(path);
         int[] output = null;
-        try{
+        try {
             long lines = Files.lines(P).count();
             BufferedReader br = new BufferedReader(new FileReader(file));
-            output =  new int[(int)lines];
-            for(int i = 0; i < lines ; i++) output[i] = Integer.parseInt(br.readLine());
-        }
-        catch (Exception e){
+            output = new int[(int) lines];
+            for (int i = 0; i < lines; i++) output[i] = Integer.parseInt(br.readLine());
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return output;
     }
 
-    public static String[] moveProccesor(String path){
+    public static String[] moveProccesor(String path) {
         Path P = Paths.get(path);
         File file = new File(path);
         String[] output = null;
-        try{
+        try {
             long lines = Files.lines(P).count();
             BufferedReader br = new BufferedReader(new FileReader(file));
-            output =  new String[(int)lines];
-            for(int i = 0; i < lines ; i++) output[i] = br.readLine();
-        }
-        catch (Exception e){
+            output = new String[(int) lines];
+            for (int i = 0; i < lines; i++) output[i] = br.readLine();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return output;
     }
 
 
-
-    public static String lifeBar(double HP , double currHp){
-        double Hpresentage = 100*currHp/HP;
+    public static String lifeBar(double HP, double currHp) {
+        double Hpresentage = 100 * currHp / HP;
         String output = "";
-        for(int i = 0;i<100-Hpresentage-1;i++){
+        for (int i = 0; i < 100 - Hpresentage - 1; i++) {
             output = output + ".";
         }
         output = output + "]";
-        for(int i=0; i<Hpresentage -1;i++){
+        for (int i = 0; i < Hpresentage - 1; i++) {
             output = "|" + output;
         }
-        output= "[" + output;
-        return  output.substring(0,48) + (int)Hpresentage + "%" + output.substring(51);
+        output = "[" + output;
+        return output.substring(0, 48) + (int) Hpresentage + "%" + output.substring(51);
 
     }
 
 
-    public static char[][] boardProccesor(File level , int height) {
+    public static char[][] boardProccesor(File level, int height) {
         char[][] result = null;
         try {
             BufferedReader br = new BufferedReader(new FileReader(level));
